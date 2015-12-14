b96cde1cac6b3cc724c27d579a1fe6d51f66209a
Austin McKinley <austinmckinley0@gmail.com>
2015-12-14 12:59:18 -0700

    Progress on Section 5. Left off at lecture 29

---
+++ todos/src/app.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var rootUrl = 'https://amber-torch-8073.firebaseio.com/';
+
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
+  render: function() {
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+        <Header />
+      </div>
+    </div>
+  }
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
